9fefe789c7b788a7338c929501bc5f4db7399440
IanSmoothStack <ian.agostini@smoothstack.com>
2021-03-22 17:41:55 -0400

    I belive this is local, sorry if this is the repo

--- src/main/java/com/caid/utopia/springsecurity/security/SecurityConfiguration.java
+++ src/main/java/com/caid/utopia/springsecurity/security/SecurityConfiguration.java
@@ -1,17 +1,24 @@
 package com.caid.utopia.springsecurity.security;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -20,27 +27,77 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	DataSource dataSource;
+
+	
+	
+	//SqlDataSource ds = new SqlDataSource();
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//	    auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-//        .dataSource(dataSource)
-//        .usersByUsernameQuery("select username, password, role_id from tbl_accounts where role_id=?")
-//        .authoritiesByUsernameQuery("select username, role_id from tbl_accounts where role_id=?")
-//    ;
-		//auth.jdbcAuthentication().dataSource(dataSource);
-		 auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("adminpass")).roles("ADMIN")
-				.and().withUser("user").password(passwordEncoder().encode("userpass")).roles("USER");
+	    auth
+	    .jdbcAuthentication()
+        .dataSource(dataSource)
+
+        .usersByUsernameQuery("select username, password, 'true' as enabled from tbl_accounts where username = ?")
+//        .authoritiesByUsernameQuery("select username, role from tbl_accounts where username=?")
+        .authoritiesByUsernameQuery(
+                "SELECT u.username, r.role_type " +
+                "FROM tbl_account_roles r, tbl_accounts u " +
+                "WHERE u.username = ? " +
+                "AND u.role_id = r.role_id"
+            )
+        .passwordEncoder(new BCryptPasswordEncoder())
+        ;
+	 
+	
+//		 auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("adminpass")).roles("ADMIN")
+//				.and().withUser("user").password(passwordEncoder().encode("userpass")).roles("USER");
+	    
+	    
+//	   System.out.println(new BCryptPasswordEncoder().encode("userpass"));
+//	   System.out.println(new BCryptPasswordEncoder().encode("adminpass"));
+	   //'userpass' = $2a$10$KJivYEkQbd8ZHt/jkCiBeeuo9cU84PiK/Peq/tXdp5sp5AtaTvpQa
+	   //'adminpass'= $2a$10$jpVmpP2nttJjpQiOx8PLre8l/zNxBgBLSx3mM.jn.xXE8.dt63cby
+	   //System.out.println( auth.jdbcAuthentication().dataSource(dataSource).getUserDetailsService().getUsersByUsernameQuery());
+	 
+	  
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		//http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
-		http.authorizeRequests().antMatchers("/public").permitAll()
-		.antMatchers("/authenticated").authenticated()
-		.antMatchers("/user").hasAnyRole("ADMIN", "USER","1","0")
-		.antMatchers("/admin").hasAnyRole("ADMIN","1")
+//		 http
+//         .logout(logout -> logout
+//           .logoutUrl("/user")
+//           .addLogoutHandler(new SecurityContextLogoutHandler())
+//         );
+//		http
+//        .logout(logout -> logout
+//          .logoutUrl("/user")
+//          .addLogoutHandler((request, response, auth) -> {
+//              for (Cookie cookie : request.getCookies()) {
+//                  String cookieName = cookie.getName();
+//                  Cookie cookieToDelete = new Cookie(cookieName, null);
+//                  cookieToDelete.setMaxAge(0);
+//                  response.addCookie(cookieToDelete);
+//              }
+//          })
+//        );
+//  
+//		http.logout();
+//		 SecurityContextHolder.clearContext();
+//		 
+//		 SecurityContextHolder.getContext().setAuthentication(null);
+		 
+		http.authorizeRequests()
+		.antMatchers("/public").permitAll()
+		.antMatchers("/authenticated").authenticated()		
+		.antMatchers("/user").hasAnyRole("ADMIN", "USER")
+		.antMatchers("/admin").hasRole("ADMIN")
 		.and().httpBasic();
+		
+//		http.logout();
+		
+		
 	}
 	
 	@Bean
