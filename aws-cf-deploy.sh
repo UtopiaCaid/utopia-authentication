8935997487acbdc0b1eb7bb55e1c5d232abffceb
IanSmoothStack <65723741+IanSmoothStack@users.noreply.github.com>
2021-04-15 11:01:45 -0400

    Update aws-cf-deploy.sh
    removed hyphen as it did not fix anything

--- aws-cf-deploy.sh
+++ aws-cf-deploy.sh
@@ -1,6 +1,6 @@
 aws cloudformation deploy --region us-east-2 --stack-name UtopiaAuthenticationMS \
 --template-file test-utopia-cftemplate --parameter-overrides ApplicationName=UtopiaAuthenticationMS \
-ECRepositoryUri=$AWS_ID/utopia-authentication:$COMMIT_HASH DBUrl=$DB_URL DBUsername=$DB_USERNAME \
+ECRepositoryUri=$AWS_ID/utopiaauthentication:$COMMIT_HASH DBUrl=$DB_URL DBUsername=$DB_USERNAME \
 DBPassword=$DB_PASSWORD ECSCluster=$UTOPIA_CLUSTER ExecutionRoleArn=$EXECUTION_ROLE_ARN \
 SubnetID=$UTOPIA_PRIVATE_SUBNET_1 TargetGroupArnDev=$TARGETGROUP_UTOPIA_AUTHENTICATION_DEV_ARN \
 TargetGroupArnProd=$TARGETGROUP_UTOPIA_AUTHENTICATION_PROD_ARN VpcId=$UTOPIA_PUBLIC_VPC_ID \
